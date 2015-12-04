42a8c269281c5533ac21070d6a270ca921686657
SJ Zhu <zsj950618@gmail.com>
2015-12-04 13:31:47 +0800

    dynamically change head

--- static/js/app.js
+++ static/js/app.js
@@ -10,49 +10,77 @@ var blogApp = angular.module('blogApp', [
     'ngRoute',
 ])
 .config(['$routeProvider', '$locationProvider',
-               function($routeProvider, $locationProvider) {
-                   $locationProvider.hashPrefix('!');
-                   $routeProvider
-                   .when('/posts/:filename', {
-                       template: '<div class="markdown-body post" simple-html="postHtml">',
-                       controller: 'PostDetailController'
-                   })
-                   .when('/', {
-                       templateUrl: 'static/templates/index.html',
-                       controller: 'IndexController'
-                   })
-                   .otherwise({
-                       redirectTo: '/'
-                   });
-               }
-
-])
-.directive('simpleHtml', function($compile){
-    return {
-        link: function($scope, $element, $attrs) {
-            var compile = function( newHTML  ) {
-                newHTML = $compile(newHTML)($scope);
-                $element.html('').append(newHTML);
-            };
-            var htmlName = $attrs.simpleHtml;
-            $scope.$watch(htmlName, function( newHTML  ) {
-                if(!newHTML) return;
-                compile(newHTML);
+        function($routeProvider, $locationProvider) {
+            $locationProvider.hashPrefix('!');
+            $routeProvider
+            .when('/posts/:filename', {
+                template: '<div class="markdown-body post" simple-html="postHtml">',
+                controller: 'PostDetailController'
+            })
+            .when('/', {
+                templateUrl: 'static/templates/index.html',
+                controller: 'IndexController'
+            })
+            .otherwise({
+                redirectTo: '/'
             });
         }
-    }
+
+])
+.directive('simpleHtml', ['$compile', 'Page',
+           function($compile, Page){
+               return {
+                   link: function($scope, $element, $attrs) {
+                       var compile = function( newHTML  ) {
+                           newHTML = $compile(newHTML)($scope);
+                           $element.html('').append(newHTML);
+                           if(!Page.getTitle()) {
+                               var heads = $element.find('h1');
+                               if(heads.length > 0) {
+                                   Page.setTitle(heads[0].textContent)
+                               }
+                           }
+                       };
+                       var htmlName = $attrs.simpleHtml;
+                       $scope.$watch(htmlName, function( newHTML  ) {
+                           if(!newHTML) return;
+                           compile(newHTML);
+                       });
+                   }
+               }
+           }
+])
+.factory('Page', function() {
+    var title;
+    return {
+        setTitle: function(newTitle) {
+            title = newTitle;
+        },
+        getTitle: function() {
+            return title;
+        }
+    };
 })
-.controller('IndexController', ['$scope', '$http',
-            function($scope, $http) {
-                $http.get('posts.json').success(function(data) {
-                    $scope.postList = data;
-                })
+.controller('PageController', ['$scope', 'Page',
+            function($scope, Page) {
+                $scope.Page = Page;
             }
 ])
-.controller('PostDetailController', ['$scope', '$routeParams', '$http',
-            function($scope, $routeParams, $http) {
+.controller('IndexController', ['$scope', '$http', 'Page',
+            function($scope, $http, Page) {
+                Page.setTitle("Home")
+                $scope.setTitle = function(title) {
+                    Page.setTitle(title);
+                };
+                $http.get('posts.json').success(function(data) {
+                    $scope.postList = data;
+                });
+            }
+])
+.controller('PostDetailController', ['$scope', '$routeParams', '$http', 'Page',
+            function($scope, $routeParams, $http, Page) {
                 $http.get('posts/'+$routeParams.filename).success(function(data) {
                     $scope.postHtml = marked(data);
-                })
+                });
             }
 ]);
