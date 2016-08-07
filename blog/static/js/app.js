d1d74c808815d53c5c101daf6fa70408d9250dae
Shengjing Zhu <zsj950618@gmail.com>
2016-08-07 14:13:47 +0800

    format style

--- blog/static/js/app.js
+++ blog/static/js/app.js
@@ -1,91 +1,93 @@
 'use strict';
 
 marked.setOptions({
-    highlight: function (code) {
-        return hljs.highlightAuto(code).value;
-    }
+  highlight: function (code) {
+    return hljs.highlightAuto(code).value;
+  }
 });
 
 var blogApp = angular.module('blogApp', [
     'ngRoute',
 ])
 .config(['$routeProvider', '$locationProvider',
-        function($routeProvider, $locationProvider) {
-            $locationProvider.html5Mode(true);
-            $locationProvider.hashPrefix('!');
-            $routeProvider
-            .when('/view/:filename', {
-                template: '<div class="markdown-body post" simple-html="postHtml">',
-                controller: 'PostDetailController'
-            })
-            .when('/', {
-                templateUrl: 'static/templates/index.html',
-                controller: 'IndexController'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-        }
+    function($routeProvider, $locationProvider) {
+      $locationProvider.html5Mode(true);
+      $locationProvider.hashPrefix('!');
+      $routeProvider
+        .when('/view/:filename', {
+          template: '<div class="markdown-body post" simple-html="postHtml">',
+          controller: 'PostDetailController'
+        })
+      .when('/', {
+        templateUrl: 'static/templates/index.html',
+        controller: 'IndexController'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+    }
 
 ])
 .directive('simpleHtml', ['$compile', '$anchorScroll', 'Page',
-           function($compile, $anchorScroll, Page){
-               return {
-                   link: function($scope, $element, $attrs) {
-                       var compile = function( newHTML  ) {
-                           newHTML = $compile(newHTML)($scope);
-                           $element.html('').append(newHTML);
-                           if(!Page.getTitle()) {
-                               var heads = $element.find('h1');
-                               if(heads.length > 0) {
-                                   Page.setTitle(heads[0].textContent)
-                               }
-                           }
-                           $anchorScroll();
-                       };
-                       var htmlName = $attrs.simpleHtml;
-                       $scope.$watch(htmlName, function( newHTML  ) {
-                           if(!newHTML) return;
-                           compile(newHTML);
-                       });
-                   }
-               }
-           }
+    function($compile, $anchorScroll, Page){
+      return {
+        link: function($scope, $element, $attrs) {
+          var compile = function( newHTML  ) {
+            newHTML = $compile(newHTML)($scope);
+            $element.html('').append(newHTML);
+            if(!Page.getTitle()) {
+              var heads = $element.find('h1');
+              if(heads.length > 0) {
+                Page.setTitle(heads[0].textContent)
+              }
+            }
+            $anchorScroll();
+          };
+          var htmlName = $attrs.simpleHtml;
+          $scope.$watch(htmlName, function( newHTML  ) {
+            if(!newHTML) return;
+            compile(newHTML);
+          });
+        }
+      }
+    }
 ])
 .factory('Page', function() {
-    var title;
-    return {
-        setTitle: function(newTitle) {
-            title = newTitle;
-        },
-        getTitle: function() {
-            return title;
-        }
-    };
+  var title;
+  return {
+    setTitle: function(newTitle) {
+      title = newTitle;
+    },
+    getTitle: function() {
+      return title;
+    }
+  };
 })
 .controller('PageController', ['$scope', '$anchorScroll', 'Page',
-            function($scope, $anchorScroll, Page) {
-                $scope.Page = Page;
-                $scope.goTo = function(anchor) {
-                    $anchorScroll(anchor);
-                }
-            }
+    function($scope, $anchorScroll, Page) {
+      $scope.Page = Page;
+      $scope.goTo = function(anchor) {
+        $anchorScroll(anchor);
+      }
+    }
 ])
 .controller('IndexController', ['$scope', '$http', 'Page',
-            function($scope, $http, Page) {
-                Page.setTitle("Home")
-                $scope.setTitle = function(title) {
-                    Page.setTitle(title);
-                };
-                $http.get('posts.json').success(function(data) {
-                    $scope.postList = data;
-                });
-            }
+    function($scope, $http, Page) {
+      Page.setTitle("Home")
+        $scope.setTitle = function(title) {
+          Page.setTitle(title);
+        };
+      $http.get('posts.json').success(function(data) {
+        $scope.postList = data;
+      });
+    }
 ])
 .controller('PostDetailController', ['$scope', '$routeParams', '$http', 'Page',
-            function($scope, $routeParams, $http, Page) {
-                $http.get('posts/'+$routeParams.filename+'.md').success(function(data) {
-                    $scope.postHtml = marked(data);
-                });
-            }
+    function($scope, $routeParams, $http, Page) {
+      $http.get('posts/'+$routeParams.filename+'.md').success(function(data) {
+        $scope.postHtml = marked(data);
+      });
+    }
 ]);
+
+// vim:ai:et:sta:ts=4:sts=4:sw=4
