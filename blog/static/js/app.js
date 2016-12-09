aade2356369ad9295f827ba8d4c6a5175555ab2b
Shengjing Zhu <zsj950618@gmail.com>
2016-12-09 22:49:52 +0800

    migrate to vuejs

--- blog/static/js/app.js
+++ blog/static/js/app.js
@@ -1,93 +1,129 @@
-'use strict';
-
 marked.setOptions({
   highlight: function (code) {
     return hljs.highlightAuto(code).value;
   }
 });
 
-var blogApp = angular.module('blogApp', [
-    'ngRoute',
-])
-.config(['$routeProvider', '$locationProvider',
-    function($routeProvider, $locationProvider) {
-      $locationProvider.html5Mode(true);
-      $locationProvider.hashPrefix('!');
-      $routeProvider
-        .when('/view/:filename', {
-          template: '<div class="markdown-body post" simple-html="postHtml">',
-          controller: 'PostDetailController'
-        })
-      .when('/', {
-        templateUrl: 'static/templates/index.html',
-        controller: 'IndexController'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-    }
+var my_fetch = function(url, cb) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = function() {
+        cb(xhr.responseText);
+    };
+    xhr.send();
+};
 
-])
-.directive('simpleHtml', ['$compile', '$anchorScroll', 'Page',
-    function($compile, $anchorScroll, Page){
-      return {
-        link: function($scope, $element, $attrs) {
-          var compile = function(newHTML) {
-            newHTML = $compile(newHTML)($scope);
-            $element.html('').append(newHTML);
-            if(!Page.getTitle()) {
-              var heads = $element.find('h1');
-              if(heads.length > 0) {
-                Page.setTitle(heads[0].textContent)
-              }
-            }
-            $anchorScroll();
-          };
-          var htmlName = $attrs.simpleHtml;
-          $scope.$watch(htmlName, function(newHTML) {
-            if(!newHTML) return;
-            compile(newHTML);
-          });
-        }
-      }
-    }
-])
-.factory('Page', function() {
-  var title;
-  return {
-    setTitle: function(newTitle) {
-      title = newTitle;
+var setTitle = function(title) {
+    document.title = title + " | SJ Zhu's Blog";
+}
+
+var Home = {
+    template: `
+        <div>
+            <div class="post" v-for="post in posts" :key="post.filename">
+                <a v-bind:href="'view/' + post.filename" class="permalink" v-on:click.prevent="go(post.filename)">#</a>
+                <h2>{{ post.name }}</h2>
+                <div class="info">Date: {{ post.date }}</div>
+            </div>
+        </div>
+    `,
+    data: function() {
+        return {posts: []};
     },
-    getTitle: function() {
-      return title;
+    mounted: function() {
+        setTitle('Home');
+        var vm = this;
+        var posts_url = "posts.json";
+        my_fetch(posts_url, function(res) {
+            posts = JSON.parse(res);
+            posts.sort(function(post_a, post_b) {
+                date_a = new Date(post_a.date);
+                date_b = new Date(post_b.date);
+                return date_a < date_b;
+            });
+            vm.posts = posts;
+        });
+    },
+    methods: {
+        go: function(filename) {
+            var href = 'view/' + filename;
+            this.$root.current_path = href;
+            window.history.pushState(
+                null,
+                null,
+                href
+            );
+        }
     }
-  };
-})
-.controller('PageController', ['$scope', '$anchorScroll', 'Page',
-    function($scope, $anchorScroll, Page) {
-      $scope.Page = Page;
-      $scope.goTo = function(anchor) {
-        $anchorScroll(anchor);
-      }
-    }
-])
-.controller('IndexController', ['$scope', '$http', 'Page',
-    function($scope, $http, Page) {
-      Page.setTitle("Home")
-        $scope.setTitle = function(title) {
-          Page.setTitle(title);
-        };
-      $http.get('posts.json').success(function(data) {
-        $scope.postList = data;
-      });
-    }
-])
-.controller('PostDetailController', ['$scope', '$routeParams', '$http', 'Page',
-    function($scope, $routeParams, $http, Page) {
-      $http.get('posts/'+$routeParams.filename+'.md').success(function(data) {
-        $scope.postHtml = marked(data);
-      });
-    }
-]);
+};
 
+Post = {
+    template: `
+        <div class="markdown-body post" v-html='postHtml'></div>
+    `,
+    data: function() {
+        return {postHtml: ''};
+    },
+    props: ['filename'],
+    mounted: function() {
+        var vm = this;
+        my_fetch('posts/'+vm.filename+'.md', function(res) {
+            vm.postHtml = marked(res);
+            var tmp = document.createElement('html');
+            tmp.innerHTML = vm.postHtml;
+            var titles = tmp.getElementsByTagName('h1');
+            if(titles.length > 0){
+                setTitle(titles[0].textContent);
+            }
+        });
+    },
+    updated: function() {
+        var hash = window.location.hash;
+        if(hash.length > 0){
+            window.location.href = window.location.href;
+        }
+    }
+};
+
+
+// base = '/blog/'
+var base = document.querySelector('base').getAttribute('href').replace(/\/$/, '');
+
+var app = new Vue({
+    el: '#content',
+    data: function() {
+        return {current_path: window.location.pathname};
+    },
+    computed:{
+        view_component: function() {
+            var path = this.current_path.replace(base, '');
+            if(!path.startsWith('/')) {
+                path = '/' + path;
+            }
+            var route_re = new RegExp('(/[^/]*).*');
+            var route_name = route_re.exec(path)[1];
+            return route_name;
+        }
+    },
+    render: function(h) {
+        if(this.view_component == '/') {
+            return h(Home);
+        } else if(this.view_component == '/view') {
+            var filename_re = new RegExp('view/([^/]*)');
+            var filename = filename_re.exec(this.current_path)[1];
+            return h(Post, {
+                props: {
+                    filename: filename
+                }
+            });
+        } else {
+            return h('h2', 'Not Found');
+        }
+    }
+
+});
+
+window.addEventListener('popstate', function(){
+  app.current_path = window.location.pathname
+});
 // vim:ai:et:sta:ts=4:sts=4:sw=4
