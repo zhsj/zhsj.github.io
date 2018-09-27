8e600ce1a8f241f514618c31fbecb3e075186221
Shengjing Zhu <i@zhsj.me>
2018-09-27 12:59:05 +0800

    [blog] retire vuejs

--- blog/static/js/app.js
+++ blog/static/js/app.js
@@ -1,8 +1,8 @@
 marked.setOptions({
-  highlight: function (code, lang) {
-    var validLang = !!(lang && hljs.getLanguage(lang));
-    return validLang ? hljs.highlight(lang, code).value : code;
-  }
+    highlight: function (code, lang) {
+        var validLang = !!(lang && hljs.getLanguage(lang));
+        return validLang ? hljs.highlight(lang, code).value : code;
+    }
 });
 
 var my_fetch = function(url, cb) {
@@ -16,115 +16,74 @@ var my_fetch = function(url, cb) {
 
 var setTitle = function(title) {
     document.title = title + " | SJ Zhu's Blog";
-}
-
-var Home = {
-    template: `
-        <div>
-            <div class="post" v-for="post in posts" :key="post.filename">
-                <a v-bind:href="'view/' + post.filename" class="permalink" v-on:click.prevent="go(post.filename)">#</a>
-                <h2>{{ post.name }}</h2>
-                <div class="info">Date: {{ post.date }}</div>
-            </div>
-        </div>
-    `,
-    data: function() {
-        return {posts: []};
-    },
-    mounted: function() {
-        setTitle('Home');
-        var vm = this;
-        var posts_url = "posts.json";
-        my_fetch(posts_url, function(res) {
-            posts = JSON.parse(res);
-            posts.sort(function(post_a, post_b) {
-                date_a = new Date(post_a.date);
-                date_b = new Date(post_b.date);
-                return date_a < date_b;
-            });
-            vm.posts = posts;
-        });
-    },
-    methods: {
-        go: function(filename) {
-            var href = 'view/' + filename;
-            this.$root.current_path = href;
-            window.history.pushState(
-                null,
-                null,
-                href
-            );
-        }
-    }
 };
 
-Post = {
-    template: `
-        <div class="markdown-body post" v-html='postHtml'></div>
-    `,
-    data: function() {
-        return {postHtml: ''};
-    },
-    props: ['filename'],
-    mounted: function() {
-        var vm = this;
-        my_fetch('posts/'+vm.filename+'.md', function(res) {
-            vm.postHtml = marked(res);
-            var tmp = document.createElement('html');
-            tmp.innerHTML = vm.postHtml;
-            var titles = tmp.getElementsByTagName('h1');
-            if(titles.length > 0){
-                setTitle(titles[0].textContent);
-            }
+var setContent = function(content) {
+    document.getElementById("content").innerHTML = content;
+};
+
+var render_home = function() {
+    var posts_url = "posts.json";
+    my_fetch(posts_url, function(res) {
+        posts = JSON.parse(res);
+        posts.sort(function(post_a, post_b) {
+            date_a = new Date(post_a.date);
+            date_b = new Date(post_b.date);
+            return date_a < date_b;
         });
-    },
-    updated: function() {
+        var content = '';
+        posts.forEach(function(post) {
+            content += '\n' +
+                '<div class="post">' +
+                '    <a href="view/' + post.filename + '" class="permalink">#</a>' +
+                '    <h2>' + post.name + '</h2>' +
+                '    <div class="info">Date: ' + post.date + '</div>' +
+                '</div>'
+        });
+        setContent(content);
+    });
+};
+
+var render_post = function(filename) {
+    my_fetch('posts/'+filename+'.md', function(res) {
+        var postHtml = marked(res);
+        var content = document.createElement('div');
+        content.classList.add('markdown-body', 'post');
+        content.innerHTML = postHtml;
+        var titles = content.getElementsByTagName('h1');
+        if(titles.length > 0){
+            setTitle(titles[0].textContent);
+        }
+        document.getElementById('content').appendChild(content);
         var hash = window.location.hash;
         if(hash.length > 0){
             window.location.href = window.location.href;
         }
-    }
+    });
 };
 
+var render_404 = function() {
+    content = '<h2>Not Found!</h2>';
+    setTitle('404');
+    setContent(content);
+};
 
 // base = '/blog/'
 var base = document.querySelector('base').getAttribute('href').replace(/\/$/, '');
 
-var app = new Vue({
-    el: '#content',
-    data: function() {
-        return {current_path: window.location.pathname};
-    },
-    computed:{
-        view_component: function() {
-            var path = this.current_path.replace(base, '');
-            if(!path.startsWith('/')) {
-                path = '/' + path;
-            }
-            var route_re = new RegExp('(/[^/]*).*');
-            var route_name = route_re.exec(path)[1];
-            return route_name;
-        }
-    },
-    render: function(h) {
-        if(this.view_component == '/') {
-            return h(Home);
-        } else if(this.view_component == '/view') {
-            var filename_re = new RegExp('view/([^/]*)');
-            var filename = filename_re.exec(this.current_path)[1];
-            return h(Post, {
-                props: {
-                    filename: filename
-                }
-            });
-        } else {
-            return h('h2', 'Not Found');
-        }
-    }
+var path = window.location.pathname.replace(base, '');
+if(!path.startsWith('/')) {
+    path = '/' + path;
+}
 
-});
+var filename_re = new RegExp('/view/([^/]*)');
+if (path === '/') {
+    render_home();
+} else if(path.startsWith('/view/')) {
+    var filename = filename_re.exec(path)[1];
+    render_post(filename.replace(/\.html$/, ''));
+} else {
+    render_404();
+}
 
-window.addEventListener('popstate', function(){
-  app.current_path = window.location.pathname
-});
 // vim:ai:et:sta:ts=4:sts=4:sw=4
